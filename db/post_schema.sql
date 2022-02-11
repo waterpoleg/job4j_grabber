993c7bba3e30b4d0661360212de89b0ca1623591
Oleg Korneev <waterpoleg@gmail.com>
2022-02-11 23:04:04 +0300

    4. Схема таблицы Post [#1731 #263373] - refactor: no limit for link length

--- db/post_schema.sql
+++ db/post_schema.sql
@@ -2,6 +2,6 @@ create table if not exists post (
     id serial primary key,
     title varchar (255),
     description text,
-    link varchar (255) unique,
+    link text unique,
     created timestamp
 );
