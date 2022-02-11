8c103fc4e9b8d45130ffc956f2cd023f6335d3e6
Oleg Korneev <waterpoleg@gmail.com>
2022-02-11 15:17:13 +0300

    2.2. Модель данных - Post. [#285211 #263158] - refactor

--- src/main/java/ru/job4j/html/Post.java
+++ src/main/java/ru/job4j/html/Post.java
@@ -5,12 +5,15 @@ import java.util.Objects;
 
 public class Post {
 
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
     public Post(int id, String title, String link, String description, LocalDateTime created) {
+        this(title, link, description, created);
         this.id = id;
-        this.title = title;
-        this.link = link;
-        this.description = description;
-        this.created = created;
     }
 
     public Post(String title, String link, String description, LocalDateTime created) {
@@ -19,12 +22,6 @@ public class Post {
         this.description = description;
         this.created = created;
     }
-
-    private int id;
-    private String title;
-    private String link;
-    private String description;
-    private LocalDateTime created;
 
     public int getId() {
         return id;
