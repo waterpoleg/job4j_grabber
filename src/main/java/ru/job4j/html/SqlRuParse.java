db1a3266c97d0d836301651f9f1a9044f354a187
Oleg Korneev <waterpoleg@gmail.com>
2022-02-11 14:11:45 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,7 +6,19 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
+import java.time.LocalDateTime;
+
 public class SqlRuParse {
+
+    private Post getSqlRuPostDetails(String link) throws Exception {
+        Document doc = Jsoup.connect(link).get();
+        String title = doc.select(".messageHeader").get(0).text();
+        String description = doc.select(".msgBody").get(1).text();
+        LocalDateTime created = new SqlRuDateTimeParser()
+                .parse(doc.select(".msgFooter").text().split(" \\[")[0]);
+        return new Post(title, link, description, created);
+    }
+
     public static void main(String[] args) throws Exception {
         for (int i = 1; i < 5; i++) {
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
@@ -21,5 +33,9 @@ public class SqlRuParse {
                 System.out.println();
             }
         }
+        Post post = new SqlRuParse().getSqlRuPostDetails("https://www.sql.ru/"
+                + "forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
+        System.out.println(post);
+        System.out.println();
     }
 }
