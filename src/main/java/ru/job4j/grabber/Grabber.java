19dbedd30a9cd557debeee096b09346c38d30138
Oleg Korneev <waterpoleg@gmail.com>
2022-02-16 18:55:21 +0300

    Revert "6. Grabber. [#289477]"
    This reverts commit 437a2806e3d4e6a0473ffb946cce63e9ce74fc1c.

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -5,8 +5,6 @@ import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
@@ -64,27 +62,6 @@ public class Grabber implements Grab {
         }
     }
 
-    public void web(Store store) {
-        new Thread(() -> {
-            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
-                while (!server.isClosed()) {
-                    Socket socket = server.accept();
-                    try (OutputStream out = socket.getOutputStream()) {
-                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        for (Post post : store.getAll()) {
-                            out.write(post.toString().getBytes());
-                            out.write(System.lineSeparator().getBytes());
-                        }
-                    } catch (IOException io) {
-                        io.printStackTrace();
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
-
     public static void main(String[] args) throws Exception {
         Grabber grab = new Grabber();
         grab.cfg();
@@ -94,6 +71,5 @@ public class Grabber implements Grab {
                 new SqlRuParse(new SqlRuDateTimeParser()),
                 store,
                 scheduler);
-        grab.web(store);
     }
 }
